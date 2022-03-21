8fe29e9a93e820d894abb0a9018d1a18428a8b75
Anoop Ramanujam <anoop.r.mundapallil@gmail.com>
2022-03-21 22:58:20 +0000

    Refactor checkbox render

--- src/components/filters/SpecFilter.js
+++ src/components/filters/SpecFilter.js
@@ -18,17 +18,35 @@ function SpecFilter(props) {
     props.setSpecFilters({ ...props.specFilters, [name]: value });
   };
 
+  function renderCheckboxes(title, options) {
+    console.log(options);
+    return (
+
+      <Grid item xs={12} sm={6} md={4}>
+        <FormLabel component="legend" justify="center" sx={{ pt: 1 }}>{title}</FormLabel>
+        <FormGroup row>
+          {
+          options.map((option) => (
+            <FormControlLabel
+              control={
+                <Checkbox checked={option.checked} onChange={handleChange} name={option.name} />
+              }
+              label={option.label}
+            />
+          ))
+
+        }
+        </FormGroup>
+      </Grid>
+    );
+  }
+
   return (
     <Box sx={{
       m: 1, p: 1, border: 1, borderColor: 'grey.400',
     }}
     >
 
-      {/* <FormControl sx={{ m: 3 }} component="fieldset" variant="standard"> */}
-      {/* <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          spacing={{ xs: 1, sm: 2, md: 4 }}
-        > */}
       <Grid
         container
         direction="row"
@@ -52,40 +70,14 @@ function SpecFilter(props) {
             }}
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <FormLabel component="legend" justify="center" sx={{ pt: 1 }}>Fuel</FormLabel>
-          <FormGroup row>
-            <FormControlLabel
-              control={
-                <Checkbox checked={petrol} onChange={handleChange} name="petrol" />
-            }
-              label="Petrol"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={diesel} onChange={handleChange} name="diesel" />
-            }
-              label="Diesel"
-            />
-          </FormGroup>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <FormLabel component="legend" justify="center" sx={{ pt: 1 }}>Transmission</FormLabel>
-          <FormGroup row>
-            <FormControlLabel
-              control={
-                <Checkbox checked={manual} onChange={handleChange} name="manual" />
-            }
-              label="Manual"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={auto} onChange={handleChange} name="auto" />
-            }
-              label="Automatic"
-            />
-          </FormGroup>
-        </Grid>
+        {renderCheckboxes('Fuel', [
+          { checked: petrol, name: 'petrol', label: 'Petrol' },
+          { checked: diesel, name: 'diesel', label: 'Diesel' }])}
+
+        {renderCheckboxes('Transmission', [
+          { checked: manual, name: 'manual', label: 'Manual' },
+          { checked: auto, name: 'auto', label: 'Automatic' }])}
+
       </Grid>
       {/* </Stack> */}
       {/* </FormControl> */}
